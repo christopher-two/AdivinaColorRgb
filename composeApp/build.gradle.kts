3988b9ccb7c8be78c34d2402dc32f8071f6191b2
Christopher <130204738+BeboCod@users.noreply.github.com>
2025-02-06 15:57:46 -0600

    readme

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import kotlin.math.sign
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -8,10 +9,10 @@ plugins {
 
 kotlin {
     jvm("desktop")
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -37,8 +38,35 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "org.colors"
+            packageName = "org.color"
             packageVersion = "1.0.0"
+            windows {
+                packageName = "AdivinaElColor"
+                packageVersion = "0.0.1"
+                description = """
+                Es una app diseñada para el aprendizaje 
+                y uso de RGB, por adivinar el color objetivo.
+            """.trimIndent()
+                vendor = "Override"
+                copyright = """
+                    Copyright © 2025 OVERRIDE https://override.com.mx/  
+                    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+                    associated documentation files (the "Software"), to deal in the Software without restriction,
+                    including without limitation the rights to use, copy, modify, merge, publish, distribute,
+                    sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+                    furnished to do so, subject to the following conditions:
+
+                    The above copyright notice and this permission notice shall be included in all copies or substantial
+                    portions of the Software.
+
+                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+                    NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+                    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+                    OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+                    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+                """.trimIndent()
+                menuGroup = "Override"
+            }
         }
     }
 }
